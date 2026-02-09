6fb9d13cb565634cb08e831cb045c45b9ca5a732
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2026-02-09 14:20:08 +0300

    chore(db): set up drizzle orm

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'drizzle-kit'
+
+export default defineConfig({
+    schema: "./src/db/schema.ts",
+    out: "./src/db/migrations",
+    driver: "pglite",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
+    verbose: true,
+    strict: true
+})
