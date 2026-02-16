bff6307a83a613978272aa0b763a1b9ba55cb127
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2026-02-16 10:30:12 +0300

    chore(db): added inventory table

---
+++ src/db/schema/asset/inventory.ts
@@ -0,0 +1,10 @@
+import { integer, timestamp } from "drizzle-orm/pg-core"
+import { house } from "../housing/house"
+import { assetSchema } from ".."
+
+export const inventory = assetSchema.table("inventory", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    houseId: integer().references(() => house.id).notNull(),
+    createdAt: timestamp().defaultNow().notNull(),
+    updatedAt: timestamp(),
+})
