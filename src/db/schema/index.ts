2a48fda9383d29415fc0dfd6c1295a1d67fe36a0
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2026-02-16 10:13:12 +0300

    chore(db): set up schemas(namespace) for different tables in postgres

---
+++ src/db/schema/index.ts
@@ -0,0 +1,5 @@
+import { pgSchema } from "drizzle-orm/pg-core"
+
+export const authSchema = pgSchema("auth")
+export const housingSchema = pgSchema("housing")
+export const assetSchema = pgSchema("asset")
