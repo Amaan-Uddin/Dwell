9d762b9388f580694bf5579c4607c81922423d13
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2026-02-17 09:56:55 +0300

    feat(db): items table and tags table have many-to-many relation via junction

--- src/db/relations/asset/item.relations.ts
+++ src/db/relations/asset/item.relations.ts
@@ -1,10 +1,12 @@
 import { relations } from "drizzle-orm"
 import { item } from "@/db/schema/asset/item"
 import { inventory } from "@/db/schema/asset/inventory"
+import { item_tag } from "@/db/schema/asset/item_tag.junction"
 
-export const itemRelations = relations(item, ({ one }) => ({
+export const itemRelations = relations(item, ({ one, many }) => ({
     inventory: one(inventory, {
         fields: [item.inventoryId],
         references: [inventory.id]
-    })
+    }),
+    itemToTags: many(item_tag)
 }))
--- src/db/relations/asset/tag.relations.ts
+++ src/db/relations/asset/tag.relations.ts
@@ -1,12 +1,12 @@
 import { relations } from "drizzle-orm"
 import { tag } from "@/db/schema/asset/tag"
-import { item } from "@/db/schema/asset/item"
 import { inventory } from "@/db/schema/asset/inventory"
+import { item_tag } from "@/db/schema/asset/item_tag.junction"
 
 export const tagRelations = relations(tag, ({ one, many }) => ({
-    items: many(item),
     inventory: one(inventory, {
         fields: [tag.inventoryId],
         references: [inventory.id]
-    })
+    }),
+    itemToTags: many(item_tag)
 }))
