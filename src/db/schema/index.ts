bd8678ab511883d4434dbce568e03313efa3f6b2
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2026-02-17 09:58:14 +0300

    feat(db): added new core schema to postgres db

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -3,3 +3,4 @@ import { pgSchema } from "drizzle-orm/pg-core"
 export const authSchema = pgSchema("auth")
 export const housingSchema = pgSchema("housing")
 export const assetSchema = pgSchema("asset")
+export const coreSchema = pgSchema("core")
