a522f11caf5ee5be7d4dce944f31f0ef331e0939
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2026-02-17 09:53:50 +0300

    feat: user table has one-to-many relation with house table

--- src/db/relations/auth/user.relations.ts
+++ src/db/relations/auth/user.relations.ts
@@ -1,7 +1,9 @@
 import { relations } from "drizzle-orm"
 import { user } from "@/db/schema/auth/user"
 import { resident } from "@/db/schema/housing/resident"
+import { house } from "@/db/schema/housing/house"
 
 export const userRelations = relations(user, ({ many }) => ({
-    resident_list: many(resident)
+    resident_list: many(resident), // this field holds a list resident id which the user has (i.e. which all houses is the user a resident of)
+    house_list: many(house)
 }))
