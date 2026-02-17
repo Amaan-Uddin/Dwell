026036775f1a3dcd5d8eecb932690e1cca00949e
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2026-02-17 09:55:20 +0300

    refactor: removed the tagId field from items table schema

--- src/db/schema/asset/item.ts
+++ src/db/schema/asset/item.ts
@@ -1,13 +1,11 @@
 import { integer, varchar, timestamp } from "drizzle-orm/pg-core"
 import { assetSchema } from ".."
 import { inventory } from "./inventory"
-import { tag } from "./tag"
 
 export const item = assetSchema.table("item", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     name: varchar({ length: 64 }).notNull(),
     inventoryId: integer().references(() => inventory.id),
-    tagId: varchar({ length: 256 }).references(() => tag.id),
     createdAt: timestamp().defaultNow().notNull(),
     updatedAt: timestamp()
 })
