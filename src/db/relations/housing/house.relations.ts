ca70dc17792d671ddb8c4f29b954681e9e41585b
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2026-02-25 14:34:23 +0300

    feat(db): updated the relations for houses and residents table accordingly to allow connection to users table

--- src/db/relations/housing/house.relations.ts
+++ src/db/relations/housing/house.relations.ts
@@ -1,7 +1,12 @@
 import { relations } from "drizzle-orm"
 import { house } from "@/db/schema/housing/house"
 import { resident } from "@/db/schema/housing/resident"
+import { user } from "@/db/schema/auth/user"
 
-export const houseRelations = relations(house, ({ many }) => ({
-    resident_list: many(resident)
+export const houseRelations = relations(house, ({ one, many }) => ({
+    resident_list: many(resident),
+    owner: one(user, {
+        fields: [house.ownedBy],
+        references: [user.id]
+    })
 }))
--- src/db/relations/housing/resident.relations.ts
+++ src/db/relations/housing/resident.relations.ts
@@ -1,10 +1,15 @@
 import { relations } from "drizzle-orm"
 import { resident } from "@/db/schema/housing/resident"
 import { house } from "@/db/schema/housing/house"
+import { user } from "@/db/schema/auth/user"
 
 export const residentRelations = relations(resident, ({ one }) => ({
     house: one(house, {
         fields: [resident.houseId],
         references: [house.id]
+    }),
+    user: one(user, {
+        fields: [resident.userId],
+        references: [user.id]
     })
 }))
