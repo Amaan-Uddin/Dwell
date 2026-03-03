99e3853a4d967eeebd2ad28eded754ae632d2ebc
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2026-03-03 11:39:28 +0300

    feat(db): add new system schema to db

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -3,4 +3,4 @@ import { pgSchema } from "drizzle-orm/pg-core"
 export const authSchema = pgSchema("auth")
 export const housingSchema = pgSchema("housing")
 export const assetSchema = pgSchema("asset")
-export const coreSchema = pgSchema("core")
+export const systemSchema = pgSchema("system")
