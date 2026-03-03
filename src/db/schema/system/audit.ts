4cefa45e434787f02aad546e70c527ac405da06c
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2026-03-03 11:42:19 +0300

    feat(db): created new audit schema for tracking changes to inventory via events

---
+++ src/db/schema/system/audit.ts
@@ -0,0 +1,19 @@
+import { integer, timestamp, jsonb, bigserial } from "drizzle-orm/pg-core"
+import { systemSchema } from ".."
+import { user } from "../auth/user"
+import { item } from "../asset/item"
+import { inventory } from "../asset/inventory"
+
+const auditEvents = systemSchema.enum("audit_events", ["ADDED", "CONSUMED", "RECONCILED", "EXPIRED", "RETURNED"])
+
+export const audit = systemSchema.table("audit", {
+    id: integer().primaryKey().generatedByDefaultAsIdentity(),
+    eventNumber: bigserial({ mode: "number" }),
+    userId: integer().references(() => user.id).notNull(),
+    itemId: integer().references(() => item.id).notNull(),
+    inventoryId: integer().references(() => inventory.id).notNull(),
+    actionType: auditEvents().notNull(),
+    qtyChange: integer().notNull(),
+    metadata: jsonb(),
+    createdAt: timestamp().defaultNow().notNull(),
+})
