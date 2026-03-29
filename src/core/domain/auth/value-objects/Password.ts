1b46efab726b4e849db7bee46bd41a71568d3c9e
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2026-03-29 12:12:12 +0300

    feat: create Password value object class with validation and factory methods

---
+++ src/core/domain/auth/value-objects/Password.ts
@@ -0,0 +1,49 @@
+interface IPasswordHasher {
+    hash(plainText: string): Promise<string>
+    compare(plainText: string, hash: string): Promise<boolean>
+}
+
+export class Password {
+    private constructor(private readonly hashValue: string) { }
+
+    static async create(value: string, hasher: IPasswordHasher): Promise<Password> {
+        this.validate(value)
+        const hashedValue = await hasher.hash(value)
+        return new Password(hashedValue)
+    }
+
+    private static validate(value: string): void {
+        if (!value || value.trim().length == 0) {
+            throw new Error("Password cannot be empty.")
+        }
+        if (value.length !== value.trim().length) {
+            throw new Error("Passwords must not contain white spaces.")
+        }
+
+        if (value.length < 8) {
+            throw new Error("Password must be at least 8 characters long.")
+        }
+
+        if (!/[A-Z]/.test(value)) {
+            throw new Error("Password must contain at least one upper case character.")
+        }
+        if (!/[a-z]/.test(value)) {
+            throw new Error("Password must contain at least one lower case character")
+        }
+        if (!/[0-9]/.test(value)) {
+            throw new Error("Password must contain at least one number.")
+        }
+    }
+
+    static reconstitute(hash: string): Password {
+        return new Password(hash)
+    }
+
+    async verify(value: string, hasher: IPasswordHasher): Promise<boolean> {
+        return await hasher.compare(value, this.hashValue)
+    }
+
+    get value(): string {
+        return this.hashValue
+    }
+}
