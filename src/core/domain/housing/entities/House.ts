56977e60cf4ca46156df0c694e87e5058b2c449c
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2026-04-01 15:08:04 +0300

    feat(house): add factory method for House class

---
+++ src/core/domain/housing/entities/House.ts
@@ -0,0 +1,49 @@
+enum HouseStatus {
+    ACTIVE = "ACTIVE",
+    ABANDONED = "ABANDONED",
+    ARCHIVED = "ARCHIVED"
+}
+
+export interface HouseProps {
+    id: string
+    name: string
+    description?: string | null
+    ownedBy: string
+    status: HouseStatus
+    createdAt: Date
+    updatedAt: Date
+}
+
+export class House {
+    private constructor(private props: HouseProps) { }
+
+    static create(params: {
+        name: string,
+        description?: string
+        createdBy: string
+    }): House {
+        if (!params.name || params.name.trim().length == 0) {
+            throw new Error("House must have a name.")
+        }
+
+        if (!params.createdBy || params.createdBy.trim().length == 0) {
+            throw new Error("Creator of house must be provided.")
+        }
+
+        const now = new Date()
+
+        return new House({
+            id: crypto.randomUUID(),
+            name: params.name,
+            description: params.description?.trim() || null,
+            ownedBy: params.createdBy,
+            status: HouseStatus.ACTIVE,
+            createdAt: now,
+            updatedAt: now
+        })
+    }
+
+    static reconstitute(props: HouseProps): House {
+        return new House(props)
+    }
+}
