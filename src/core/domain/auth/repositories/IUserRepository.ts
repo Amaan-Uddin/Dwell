aa5ff4fc4f5f088f458c464b4a86933665669ea4
Mohammed Amaan Uddin <mohdamaanuddin100@gmail.com>
2026-04-02 16:29:45 +0300

    feat(user): add null return type to methods that return single entity

--- src/core/domain/auth/repositories/IUserRepository.ts
+++ src/core/domain/auth/repositories/IUserRepository.ts
@@ -3,11 +3,11 @@ export interface IUserRepository {
     save(user: User): Promise<User>
     findById(id: string): Promise<User | null>
 
-    findByEmail(email: string): Promise<User>
-    findByExternalAuthId(externalAuthId: string): Promise<User>
+    findByEmail(email: string): Promise<User | null>
+    findByExternalAuthId(externalAuthId: string): Promise<User | null>
 
     findActiveUsers(): Promise<User[]>
-    findUsersByRole(role: UserRoles): Promise<User[]>
+    findByRole(role: UserRoles): Promise<User[]>
 
     delete(id: string): Promise<void>
 }
