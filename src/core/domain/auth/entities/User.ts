dfb0ae360494743a56b2325f1bf45ef236084355
Amaan <mohdamaanuddin100@gmail.com>
2026-04-20 14:51:55 +0300

    feat: update user class entity
    - strict and only necessary condition checks
    - remove async methods
    - defined state transition method names

--- src/core/domain/auth/entities/User.ts
+++ src/core/domain/auth/entities/User.ts
@@ -1,5 +1,5 @@
 import { Email } from "../value-objects/Email"
-import { Password, IPasswordHasher } from "../value-objects/Password"
+import { Password } from "../value-objects/Password"
 
 enum UserStatus {
     ACTIVE = "ACTIVE",
@@ -30,65 +30,46 @@ export interface UserProps {
 export class User {
     private constructor(private props: UserProps) { }
 
-    // factory method for creating a user
-    static async create(params: {
+    static create(params: {
         firstName: string,
         lastName?: string,
-        email: string,
-        password?: string,
+        email: Email,
+        password?: Password,
         externalAuthId?: string,
-        role?: string
-    }, hasher: IPasswordHasher): Promise<User> {
-
-        // firstName is required so we must ensure it exist and check its length after trimming
+        role?: UserRoles
+    }): User {
         if (!params.firstName?.trim()) {
             throw new Error("First name must not be empty.")
         }
 
-        // lastName is optional, so we check if it exist, if so then check length after trimming
-        // if (params.lastName && params.lastName.trim().length === 0) {
-        //     throw new Error("Last name cannot be empty.")
-        // }
-
-        const email = Email.create(params.email)
-        const password = params.password ? await Password.create(params.password, hasher) : undefined
+        if (!params.password && !params.externalAuthId) {
+            throw new Error("Password or user auth ID must be provided.")
+        }
 
         const now = new Date()
-
-        // check if role exist and then verify whether the role is included in the UserRoles enum.
-        // we are asserting the params.role as UserRole to remove the typescript error of string not being assignable to UserRoles
-        if (params.role && !Object.values(UserRoles).includes(params.role as UserRoles)) {
-            throw new Error("Invalid user role provided.")
-        }
 
         return new User(
             {
                 id: crypto.randomUUID(),
                 firstName: params.firstName,
                 lastName: params.lastName,
-                email: email,
-                password: password,
+                email: params.email,
+                password: params.password,
                 externalAuthId: params.externalAuthId,
                 status: UserStatus.ACTIVE,
-                role: params.role as UserRoles,
+                role: params.role ?? UserRoles.USER,
                 createdAt: now,
                 updatedAt: now
             }
         )
     }
 
-    // factory method for guest users
     static createGuest(sessionId: string): User {
-        // guest users will not have many of the UserProps so we can default the values in core logic
         const now = new Date()
 
         return new User({
             id: sessionId,
             firstName: "Guest",
-            lastName: undefined,
-            email: undefined,
-            password: undefined,
-            externalAuthId: undefined,
             status: UserStatus.ACTIVE,
             role: UserRoles.GUEST,
             createdAt: now,
@@ -105,6 +86,10 @@ export class User {
     // Getters
     get id(): string {
         return this.props.id
+    }
+
+    get firstName(): string {
+        return this.props.firstName
     }
 
     get fullName(): string | undefined {
@@ -141,53 +126,15 @@ export class User {
         return this.props.status === UserStatus.DELETED
     }
 
-    // Business methods
-    // methods for change of state of password
-    async verifyPassword(plainText: string, hasher: IPasswordHasher): Promise<boolean> {
-        if (!this.props.password) {
-            return false
-        }
-        return await this.props.password.verify(plainText, hasher)
-    }
-
-    async changePassword(oldPassword: string, newPassword: string, hasher: IPasswordHasher): Promise<void> {
-        if (!this.props.password) {
-            throw new Error("User does not have a password.")
-        }
-
-        if (!(await this.verifyPassword(oldPassword, hasher))) {
-            throw new Error("Incorrect password.")
-        }
-
-        const password = await Password.create(newPassword, hasher)
-
-        this.props.password = password
-        this.markAsUpdated()
-    }
-
-    async resetPassword(newPassword: string, hasher: IPasswordHasher): Promise<void> {
-        if (!this.props.password) {
-            throw new Error("User does not have a password.")
-        }
-
-        if (await this.verifyPassword(newPassword, hasher)) {
-            throw new Error("New password cannot be same as old password.")
-        }
-
-        const password = await Password.create(newPassword, hasher)
-
-        this.props.password = password
-        this.markAsUpdated()
-    }
 
     // transforming guest to user
-    async promoteGuestToUser(params: {
+    promoteGuestToUser(params: {
         firstName: string,
         lastName?: string,
-        email: string,
-        password?: string,
+        email: Email,
+        password?: Password,
         externalAuthId?: string,
-    }, hasher: IPasswordHasher): Promise<void> {
+    }): void {
         if (!this.isGuest()) {
             throw new Error("Not a guest, cannot be promoted to a user.")
         }
@@ -195,23 +142,22 @@ export class User {
         if (!params.firstName?.trim()) {
             throw new Error("First name must not be empty.")
         }
+
+        if (!params.password && !params.externalAuthId) {
+            throw new Error("Password or external auth ID must be provided.")
+        }
+
         this.props.firstName = params.firstName
-
-        // if (params.lastName && params.lastName.trim().length === 0) {
-        //     throw new Error("Last name cannot be empty.")
-        // }
         this.props.lastName = params.lastName
-
-        this.props.email = Email.create(params.email)
-        this.props.password = params.password ? await Password.create(params.password, hasher) : undefined
+        this.props.email = params.email
+        this.props.password = params.password
         this.props.externalAuthId = params.externalAuthId
-
         this.props.role = UserRoles.USER
 
         this.markAsUpdated()
     }
 
-    promoteToAdmin(): void {
+    promoteUserToAdmin(): void {
         if (!this.isUser()) {
             if (this.isAdmin()) {
                 throw new Error("User is already an admin.")
@@ -225,7 +171,7 @@ export class User {
         this.markAsUpdated()
     }
 
-    demoteToUser(): void {
+    demoteAdminToUser(): void {
         if (!this.isAdmin()) {
             if (this.isUser()) {
                 throw new Error("Already has user role.")
