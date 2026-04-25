1b79a72cfabf3a9958d40b3a9a81f5c3980e3b14
Amaan <mohdamaanuddin100@gmail.com>
2026-04-25 17:43:08 +0300

    feat: rework setup file for integration testing
    - remove the schema-per-test
    - add before all to set up connection with test db
    - export db instance

--- test/setup.ts
+++ test/setup.ts
@@ -1,31 +1,18 @@
 import { Pool } from "pg"
 import { drizzle } from "drizzle-orm/node-postgres"
 import { migrate } from "drizzle-orm/node-postgres/migrator"
-import { randomUUID } from "crypto"
+import * as schema from "@/db/schema"
+import { beforeAll, afterAll } from "vitest"
 
-export async function createTestDb() {
-    // generate a schema name for our test db
-    const schema = `test_${randomUUID().slice(0, 8)}`
+const pool = new Pool({
+    connectionString: process.env.TEST_DB_URL
+})
+export const db = drizzle({ client: pool, schema: schema })
 
-    const pool = new Pool({
-        connectionString: process.env.TEST_DB_URL
-    })
-    // create a test schema in our test db
-    await pool.query(`create schema if not exist ${schema}`)
-    // tell postgres to search for tables and db objects in schema by setting a search path(s)
-    await pool.query(`set search_path to ${schema}`)
+beforeAll(async () => {
+    await migrate(db, { migrationsFolder: "./src/db/migration" })
+})
 
-    // create a drizzle db client using postgres connection pool 
-    const db = drizzle(pool)
-    // apply all migrations to test db
-    await migrate(db, { migrationsFolder: "../src/db/migrations" })
-
-    // return drizzle client instance and a cleanup function to drop schema and all its tables and db objects
-    return {
-        db,
-        cleanup: async () => {
-            await pool.query(`drop schema ${schema} cascade`)
-            await pool.end()
-        }
-    }
-}
+afterAll(async () => {
+    await pool.end()
+})
