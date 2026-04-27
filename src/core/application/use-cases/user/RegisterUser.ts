9b2e89e383a20ae7d57a698f36ba418c388b81d9
Amaan <mohdamaanuddin100@gmail.com>
2026-04-27 16:34:36 +0300

    feat(user): implement register user use-case

---
+++ src/core/application/use-cases/user/RegisterUser.ts
@@ -0,0 +1,44 @@
+import { IUserRepository } from "@/core/domain/auth/repositories/IUserRepository"
+import { RegisterUserDTO } from "../../dtos/user/RegisterUserDTO"
+import { UserResponseDTO } from "../../dtos/user/UserResponseDTO"
+import { UserMapper } from "../../mappers/user/UserMapper"
+import { User } from "@/core/domain/auth/entities/User"
+import { Email } from "@/core/domain/auth/value-objects/Email"
+import { IPasswordHasher, Password } from "@/core/domain/auth/value-objects/Password"
+
+export class RegisterUser {
+    constructor(
+        private userRepo: IUserRepository,
+        private hasher: IPasswordHasher
+    ) { }
+
+    async execute(dto: RegisterUserDTO): Promise<UserResponseDTO> {
+        // find if an user with same email id exists or not
+        const emailTaken = await this.userRepo.findByEmail(dto.email)
+        if (emailTaken) throw new Error("Email already in use.")
+
+        // create a validated email value object
+        const email = Email.create(dto.email)
+
+        // validate the password
+        Password.validate(dto.password)
+        // then hash the password using a hasher
+        const passwordHash = await this.hasher.hash(dto.password)
+        // finally create a password value object
+        const password = Password.createFromHash(passwordHash)
+
+        // now we create a user object that performs validation on domain level 
+        const user = User.create({
+            firstName: dto.firstName,
+            lastName: dto.lastName,
+            email: email,
+            password: password
+        })
+
+        // save the user to our database
+        const savedUser = await this.userRepo.save(user)
+
+        // return a JSON compatible response back to client
+        return UserMapper.toDTO(savedUser)
+    }
+}
