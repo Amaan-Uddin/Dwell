5d69bea856d81e75a659ac6719c61af4b49977d1
Amaan <mohdamaanuddin100@gmail.com>
2026-04-29 16:31:19 +0300

    refactor: change the return type for delete operation in user repository interface

--- src/core/domain/auth/repositories/IUserRepository.ts
+++ src/core/domain/auth/repositories/IUserRepository.ts
@@ -11,6 +11,6 @@ export interface IUserRepository {
     findByRole(role: UserRoles): Promise<User[]>
 
     forceDelete(id: string): Promise<void>
-    delete(user: User): Promise<string>
+    delete(user: User): Promise<User>
     findDeletedUsers(): Promise<User[]>
 }
