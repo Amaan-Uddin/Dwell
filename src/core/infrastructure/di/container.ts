3d11e4ab8e6d1cadd870c23ea7c0b4cef618d245
Amaan <mohdamaanuddin100@gmail.com>
2026-04-29 16:36:06 +0300

    chore(di): inject delete user user-case

--- src/core/infrastructure/di/container.ts
+++ src/core/infrastructure/di/container.ts
@@ -2,8 +2,10 @@ import { RegisterUser } from "@/core/application/use-cases/user/RegisterUser"
 import { DrizzleUserRepository } from "../persistence/drizzle/repositories/DrizzleUserRepository"
 import { PasswordHasher } from "../auth-services/PasswordHasher"
 import { db } from "@/db"
+import { DeleteUser } from "@/core/application/use-cases/user/DeleteUser"
 
 const hasher = new PasswordHasher()
 
 export const userRepository = new DrizzleUserRepository(db)
 export const registerUserUseCase = new RegisterUser(userRepository, hasher)
+export const deleteUserUseCase = new DeleteUser(userRepository)
