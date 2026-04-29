3fec2028108807ed673a797b7df21747a68a888b
Amaan <mohdamaanuddin100@gmail.com>
2026-04-29 16:33:53 +0300

    feat(user): add delete user user-case

---
+++ src/core/application/use-cases/user/DeleteUser.ts
@@ -0,0 +1,22 @@
+import { IUserRepository } from "@/core/domain/auth/repositories/IUserRepository";
+import { DeleteUserDTO } from "../../dtos/user/DeleteUserDTO";
+import { DeleteUserResponseDTO } from "../../dtos/user/UserResponseDTO";
+import { UserMapper } from "../../mappers/user/UserMapper";
+
+export class DeleteUser {
+    constructor(private userRepo: IUserRepository) { }
+
+    async execute(dto: DeleteUserDTO): Promise<DeleteUserResponseDTO> {
+        // find the user by the id provided
+        const user = await this.userRepo.findById(dto.id)
+        if (!user) throw new Error(`User with ID=${dto.id} does not exist.`)
+
+        // if user exists then invoke the deleteUser() method to update the object state
+        user.deleteUser()
+        // now persist the updated user state to database
+        const deletedUser = await this.userRepo.delete(user)
+
+        // return the deleted user response to client
+        return UserMapper.toDeleteUserDTO(deletedUser)
+    }
+}
