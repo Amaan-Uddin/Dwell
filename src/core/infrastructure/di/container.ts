4227c8c2c7db689ef76b8199c36ad1904a344082
Amaan <mohdamaanuddin100@gmail.com>
2026-04-30 16:22:59 +0300

    chore(di): inject create guest use case and session service

--- src/core/infrastructure/di/container.ts
+++ src/core/infrastructure/di/container.ts
@@ -3,9 +3,15 @@ import { DrizzleUserRepository } from "../persistence/drizzle/repositories/Drizz
 import { PasswordHasher } from "../auth-services/PasswordHasher"
 import { db } from "@/db"
 import { DeleteUser } from "@/core/application/use-cases/user/DeleteUser"
+import { CreateGuest } from "@/core/application/use-cases/user/CreateGuest"
+import { SessionService } from "../persistence/drizzle/services/DrizzleSessionService"
 
 const hasher = new PasswordHasher()
 
 export const userRepository = new DrizzleUserRepository(db)
+
+export const sessionService = new SessionService(db)
+
 export const registerUserUseCase = new RegisterUser(userRepository, hasher)
 export const deleteUserUseCase = new DeleteUser(userRepository)
+export const createGuestUseCase = new CreateGuest(userRepository, sessionService)
