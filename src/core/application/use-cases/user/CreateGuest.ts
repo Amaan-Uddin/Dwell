841615e0c628aa5df6e8add73e75c8ffaa54c58d
Amaan <mohdamaanuddin100@gmail.com>
2026-04-30 16:18:46 +0300

    feat: add create guest use case

---
+++ src/core/application/use-cases/user/CreateGuest.ts
@@ -0,0 +1,24 @@
+import { User } from "@/core/domain/auth/entities/User";
+import { ISessionService } from "@/core/infrastructure/persistence/drizzle/services/ISessionService";
+import { IUserRepository } from "@/core/domain/auth/repositories/IUserRepository";
+import { UserMapper } from "../../mappers/user/UserMapper";
+import { CreateGuestResponseDTO } from "../../dtos/user/CreateGuest/CreateGuestResponseDTO";
+
+export class CreateGuest {
+    constructor(private userRepo: IUserRepository, private sessionService: ISessionService) { }
+
+    async execute(): Promise<CreateGuestResponseDTO> {
+        // create a guest user object
+        const guest = User.createGuest()
+
+        // save the guest to our db
+        await this.userRepo.save(guest)
+
+        // create a session for the guest
+        const { sessionId } = await this.sessionService.createSession(guest.id)
+
+        // return the guest ID and session ID back to client
+        return UserMapper.toCreateGuestDTO(guest, sessionId)
+    }
+
+}
