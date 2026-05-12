3942477b540303017fff06af7c9f2b7efd87cdda
Amaan <mohdamaanuddin100@gmail.com>
2026-05-12 15:38:06 +0300

    feat(test): redefine config for vitest, now allows for segregating tests into unit and integration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,33 @@
-import { defineConfig } from "vitest/config"
+import { defineConfig, defineProject } from "vitest/config"
 import path from "path"
 
-export default defineConfig({
-    test: {
-        setupFiles: ["./test/setup.ts"]
-    },
+const sharedConfig = {
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src")
         }
     }
+}
+
+export default defineConfig({
+    test: {
+        projects: [
+            defineProject({
+                test: {
+                    name: "unit",
+                    include: ["test/unit/**/*.test.ts"]
+                },
+                ...sharedConfig
+            }),
+            defineProject({
+                test: {
+                    name: "integration",
+                    include: ["test/integration/*.test.ts"],
+                    globalSetup: "test/globalSetup.ts",
+                    pool: "forks"
+                },
+                ...sharedConfig
+            }),
+        ]
+    },
 })
