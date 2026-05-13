96372ff74885aa3e78742a1a194a3b519e6dd877
Amaan <mohdamaanuddin100@gmail.com>
2026-05-13 23:51:26 +0300

    chore(di): add clerk service

--- src/core/infrastructure/di/container.ts
+++ src/core/infrastructure/di/container.ts
@@ -6,12 +6,15 @@ import { DeleteUser } from "@/core/application/use-cases/user/DeleteUser"
 import { CreateGuest } from "@/core/application/use-cases/user/CreateGuest"
 import { SessionService } from "../persistence/drizzle/services/DrizzleSessionService"
 import { GetUser } from "@/core/application/use-cases/user/GetUser"
+import { createClerkClient } from "@clerk/backend"
+import { ClerkService } from "../auth-services/clerk/ClerkService"
 
 const hasher = new PasswordHasher()
+const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY })
 
 export const userRepository = new DrizzleUserRepository(db)
-
 export const sessionService = new SessionService(db)
+export const clerkService = new ClerkService(clerkClient)
 
 export const registerUserUseCase = new RegisterUser(userRepository, hasher)
 export const deleteUserUseCase = new DeleteUser(userRepository)
