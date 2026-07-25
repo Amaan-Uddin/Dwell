2443391e5a80735e8f563e99d22063a9abfb64d6
Amaan <mohdamaanuddin100@gmail.com>
2026-07-25 20:40:54 +0300

    feat(get-user): now the get user use-case executes method to retrieve user based on externalAuthId or clerk session, and if the user does not exist we perform JIT provisioning to create user at the moment

--- src/core/application/dtos/user/GetUser/GetUserDTO.ts
+++ src/core/application/dtos/user/GetUser/GetUserDTO.ts
@@ -6,6 +6,10 @@ export const GetUserSchema = z.object({
 export const GetUserFromSessionSchema = z.object({
     sessionId: z.string()
 })
+export const GetUserFromClerkSchema = z.object({
+    externalAuthId: z.string()
+})
 
 export type GetUserDTO = z.infer<typeof GetUserSchema>
 export type GetUserFromSessionDTO = z.infer<typeof GetUserFromSessionSchema>
+export type GetUserFromClerkDTO = z.infer<typeof GetUserFromClerkSchema>
--- src/core/application/use-cases/user/GetUser.ts
+++ src/core/application/use-cases/user/GetUser.ts
@@ -1,30 +1,53 @@
 import { IUserRepository } from "@/core/domain/auth/repositories/IUserRepository"
 import { ISessionService } from "@/core/infrastructure/persistence/drizzle/services/ISessionService"
 import { GetUserResponseDTO } from "../../dtos/user/GetUser/GetUserResponseDTO"
-import { GetUserDTO, GetUserFromSessionDTO } from "../../dtos/user/GetUser/GetUserDTO"
+import { GetUserDTO, GetUserFromClerkDTO, GetUserFromSessionDTO } from "../../dtos/user/GetUser/GetUserDTO"
 import { UserMapper } from "../../mappers/user/UserMapper"
+import { IClerkService } from "@/core/infrastructure/auth-services/clerk/IClerkService"
+import { User } from "@/core/domain/auth/entities/User"
+import { Email } from "@/core/domain/auth/value-objects/Email"
 
 export class GetUser {
     constructor(
         private userRepo: IUserRepository,
-        private sessionService: ISessionService
+        private sessionService: ISessionService,
+        private clerkService: IClerkService
     ) { }
 
-    // async executeFromClerkSession(): Promise<GetUserResponseDTO | null> { }
+    async executeFromClerkSession(dto: GetUserFromClerkDTO): Promise<GetUserResponseDTO> {
+        let user = await this.userRepo.findByExternalAuthId(dto.externalAuthId)
+
+        // performing JIT=Just-In-Time Provisioning, we are creating a local user in our db at the moment we need it, rather than upfront
+        if (!user) {
+            const clerkProfile = await this.clerkService.fetchUser(dto.externalAuthId)
+
+            const clerkUserEmail = clerkProfile.primaryEmailAddress?.emailAddress
+            if (!clerkUserEmail) throw new Error(`Clerk user ${dto.externalAuthId} has no email set as primary email.`)
+            user = User.create({
+                firstName: clerkProfile.firstName ?? "Unknown",
+                email: Email.create(clerkProfile.primaryEmailAddress?.emailAddress),
+                externalAuthId: dto.externalAuthId
+            })
+
+            await this.userRepo.save(user)
+        }
+
+        return UserMapper.toGetUserDTO(user)
+    }
 
     async executeFromSessionId(dto: GetUserFromSessionDTO): Promise<GetUserResponseDTO | null> {
         const session = await this.sessionService.validateSession(dto.sessionId)
         if (!session) return null
 
         const user = await this.userRepo.findById(session.userId)
-        if (!user) return null
+        if (!user) throw new Error(`User with ID=${session.userId} does not exist.`)
 
         return UserMapper.toGetUserDTO(user)
     }
 
     async execute(dto: GetUserDTO): Promise<GetUserResponseDTO | null> {
         const user = await this.userRepo.findById(dto.userId)
-        if (!user) return null
+        if (!user) throw new Error(`User with ID=${dto.userId} does not exist.`)
 
         return UserMapper.toGetUserDTO(user)
     }
