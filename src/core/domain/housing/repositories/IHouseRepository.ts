134bc8075ecae3013ae8d51e6ee61d18d2cbb792
Amaan <mohdamaanuddin100@gmail.com>
2026-07-27 16:02:54 +0300

    feat(house): implement house repository methods via drizzle-orm

--- src/core/domain/housing/repositories/IHouseRepository.ts
+++ src/core/domain/housing/repositories/IHouseRepository.ts
@@ -1,9 +1,10 @@
 import { House, HouseStatus } from "../entities/House";
 
 export interface IHouseRepository {
-    save(house: House): Promise<House>
-    findById(id: string): Promise<House | null>
+    save(house: House): Promise<House> // house creation and updates
+    findById(id: string): Promise<House | null> // fetching house and house details
 
+    // admin queries
     findByOwner(ownerId: string): Promise<House[]>
     findByStatus(status: HouseStatus): Promise<House[]>
 }
---
+++ src/core/infrastructure/persistence/drizzle/repositories/DrizzleHouseRepository.ts
@@ -0,0 +1,91 @@
+import { House, HouseStatus } from "@/core/domain/housing/entities/House"
+import { IHouseRepository } from "@/core/domain/housing/repositories/IHouseRepository"
+import { HouseSelectType, house as HouseTb } from "@/db/schema/housing/house"
+import { Database } from "@/db"
+import { drizzleErrorLogger } from "../utils"
+import { eq } from "drizzle-orm"
+
+export class DrizzleHouseRepository implements IHouseRepository {
+    constructor(private readonly db: Database) { }
+
+    async save(house: House): Promise<House> {
+        const db_row = this.toPersistence(house)
+        try {
+            const row = await this.db.insert(HouseTb).values(db_row).onConflictDoUpdate({
+                target: HouseTb.id,
+                set: { ...db_row, createdAt: undefined }
+            }).returning()
+            return this.toDomain(row[0])
+        } catch (error) {
+            drizzleErrorLogger(error, { operation: "save", house: house })
+            throw new Error("Failed to save house data to db.", { cause: error })
+        }
+    }
+
+    async findById(id: string): Promise<House | null> {
+        try {
+            if (!id.trim()) throw new Error("Cannot find house without an ID.")
+            const result = await this.db.select().from(HouseTb).where(eq(HouseTb.id, id)).limit(1)
+            return result[0] ? this.toDomain(result[0]) : null
+        } catch (error) {
+            drizzleErrorLogger(error, { operation: "findById", id: id })
+            throw new Error(`Failed to fetch house with ID=${id}.`, { cause: error })
+        }
+    }
+
+    async findByOwner(ownerId: string): Promise<House[]> {
+        try {
+            if (!ownerId.trim()) throw new Error("Cannot find house without ownerId.")
+            const result = await this.db.select().from(HouseTb).where(eq(HouseTb.ownedBy, ownerId))
+            return result.map((row) => this.toDomain(row))
+        } catch (error) {
+            drizzleErrorLogger(error, { operation: "findByOwner", ownerId: ownerId })
+            throw new Error(`Failed to fetch houses owned by ID=${ownerId}`, { cause: error })
+        }
+    }
+
+    async findByStatus(status: HouseStatus): Promise<House[]> {
+        try {
+            if (!status.trim()) throw new Error("Cannot find house without a status.")
+            const result = await this.db.select().from(HouseTb).where(eq(HouseTb.status, status))
+            return result.map((row) => this.toDomain(row))
+        } catch (error) {
+            drizzleErrorLogger(error, { operation: "findByStatus", status: status })
+            throw new Error(`Failed to fetch houses with status=${status}`, { cause: error })
+        }
+    }
+
+    private toDomain(db_house: HouseSelectType): House {
+
+        if (!Object.values(HouseStatus).includes(db_house.status as HouseStatus)) {
+            throw new Error(`Database corruption detected: Invalid house status ${db_house.status}`)
+        }
+
+        return House.reconstitute({
+            id: db_house.id,
+            name: db_house.name,
+            description: db_house.description,
+            status: db_house.status as HouseStatus,
+            ownedBy: db_house.ownedBy,
+            createdAt: db_house.createdAt,
+            updatedAt: db_house.updatedAt,
+            abandonedAt: db_house.abandonedAt,
+            archivedAt: db_house.archivedAt
+        })
+    }
+
+    private toPersistence(house: House): HouseSelectType {
+        const houseObj = house.toObject()
+        return {
+            id: houseObj.id,
+            name: houseObj.name,
+            description: houseObj.description,
+            status: houseObj.status,
+            ownedBy: houseObj.ownedBy,
+            createdAt: houseObj.createdAt,
+            updatedAt: houseObj.updatedAt,
+            abandonedAt: houseObj.abandonedAt,
+            archivedAt: houseObj.archivedAt
+        }
+    }
+} 
