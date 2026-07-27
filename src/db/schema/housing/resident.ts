f4aedf82c0b38015179bf453a8f27664a93e335f
Amaan <mohdamaanuddin100@gmail.com>
2026-07-27 11:13:02 +0300

    feat(resident schema): add a remove status enum to the table to track if the user was temporarily or permanently removed from the house

--- src/db/schema/housing/resident.ts
+++ src/db/schema/housing/resident.ts
@@ -10,12 +10,19 @@ import { housingSchema } from "./schema"
  */
 const residentStatus = housingSchema.enum("resident_status", ["ACTIVE", "LEFT", "REMOVED"])
 
+/**
+ * TEMP - remove status for the resident, meta-data indicating user was removed temporarily
+ * PERM - resident was removed permanently from the house
+ */
+const removeStatus = housingSchema.enum("resident_remove_status", ["TEMP", "PERM"])
+
 export const resident = housingSchema.table("resident", {
     id: uuid().primaryKey(),
     userId: uuid().references(() => user.id).notNull(),
     houseId: uuid().references(() => house.id).notNull(),
     status: residentStatus().notNull(),
     rejoinedCount: integer().default(0).notNull(),
+    removedStatus: removeStatus(),
     createdAt: timestamp().defaultNow().notNull(),
     updatedAt: timestamp().notNull(),
     leftAt: timestamp(),
