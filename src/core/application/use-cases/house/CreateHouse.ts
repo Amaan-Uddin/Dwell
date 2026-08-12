6f2dac751d9627c943376eba2f465effa560aa95
Amaan <mohdamaanuddin100@gmail.com>
2026-08-12 16:43:21 +0300

    feat: create the inventory for the house during house initialization

--- src/core/application/use-cases/house/CreateHouse.ts
+++ src/core/application/use-cases/house/CreateHouse.ts
@@ -1,10 +1,10 @@
-import { CreateHouseDTO } from "../../dtos/house/CreateHouseDTO"
-import { CreateHouseResponseDTO } from "../../dtos/house/CreateHouseDTO"
+import { CreateHouseDTO, CreateHouseResponseDTO } from "../../dtos/house/CreateHouseDTO"
 import { IUserRepository } from "@/core/domain/auth/repositories/IUserRepository"
 import { House } from "@/core/domain/housing/entities/House"
 import { HouseMapper } from "../../mappers/HouseMapper"
 import { IUnitOfWork } from "@/core/domain/shared/IUnitOfWork"
 import { Resident, ResidentRole } from "@/core/domain/housing/entities/Resident"
+import { Inventory } from "@/core/domain/asset/entities/inventory"
 
 export class CreateHouse {
     constructor(private userRepo: IUserRepository, private uow: IUnitOfWork) { }
@@ -30,17 +30,21 @@ export class CreateHouse {
         const MAX_HOUSE = 5
 
         // everything inside here runs in ONE transaction
-        const savedHouse = await this.uow.execute(async ({ houseRepo, residentRepo }) => {
+        const savedHouse = await this.uow.execute(async ({ houseRepo, residentRepo, inventoryRepo }) => {
             const houseCount = await houseRepo.findHouseCount({ ownerId: dto.userId, forUpdate: true })
             if (houseCount >= MAX_HOUSE) {
                 throw new Error(`User already has reached the max limit of ${MAX_HOUSE} houses.`, { cause: "MAX_HOUSE_LIMIT" })
             }
 
-            // create a house domain object
+            // create a house object and an inventory object for the house, and save them to the db
             const house = House.create({ name: dto.name, description: dto.description, ownedBy: dto.userId })
+            const inventory = Inventory.create({ houseId: house.id })
 
             // save the house to our db using the transaction
             const savedHouse = await houseRepo.save({ house })
+
+            // save the inventory to our db using the transaction
+            await inventoryRepo.save({ inventory })
 
             // we also have to create the house owner resident record and save it
             const resident = Resident.create({ userId: dto.userId, houseId: savedHouse.id, role: ResidentRole.OWNER })
